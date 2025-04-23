4fba5b6e1d715c6c8286b693ed24b9ab05383198
Gleam <gleam@antigravity.kr>
2025-04-23 16:16:00 +0900

    Add Icons and modify icon urls

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<Image className="dark:invert" src="https://nextjs.org/icons/next.svg" alt="Next.js logo" width={180} height={38} priority />
+				<Image className="dark:invert" src="/icons/next.svg" alt="Next.js logo" width={180} height={38} priority />
 				<ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
 					<li className="mb-2">
 						Get started by editing <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">app/page.tsx</code>
@@ -20,7 +20,7 @@ export default function Home() {
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<Image className="dark:invert" src="https://nextjs.org/icons/vercel.svg" alt="Vercel logomark" width={20} height={20} />
+						<Image className="dark:invert" src="/icons/vercel.svg" alt="Vercel logomark" width={20} height={20} />
 						Deploy now
 					</a>
 					<a
@@ -50,7 +50,7 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<Image aria-hidden src="https://nextjs.org/icons/file.svg" alt="File icon" width={16} height={16} />
+					<Image aria-hidden src="/icons/file.svg" alt="File icon" width={16} height={16} />
 					Learn
 				</a>
 				<a
@@ -59,7 +59,7 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<Image aria-hidden src="https://nextjs.org/icons/window.svg" alt="Window icon" width={16} height={16} />
+					<Image aria-hidden src="/icons/window.svg" alt="Window icon" width={16} height={16} />
 					Examples
 				</a>
 				<a
@@ -68,7 +68,7 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<Image aria-hidden src="https://nextjs.org/icons/globe.svg" alt="Globe icon" width={16} height={16} />
+					<Image aria-hidden src="/icons/globe.svg" alt="Globe icon" width={16} height={16} />
 					Go to nextjs.org →
 				</a>
 			</footer>
